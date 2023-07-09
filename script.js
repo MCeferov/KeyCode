00b4d0b298d6d51e681fd0ba976e620a96b01198
MCeferov <misomanxid@gmail.com>
2023-07-09 10:44:07 +0400

    KeyCode

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+const insert = document.getElementById("insert");
+const keyPressEl = document.querySelector('.keyPress');
+const keysEl = document.querySelector('.keys');
+const keys = document.querySelectorAll('.key span');
+
+const [key , code, keyCode] = keys
+window.addEventListener('keyup', (event) => {
+    keyPressEl.classList.add('hidden');
+    keysEl.classList.remove('hidden');
+
+    key.textContent = event.key === `` ? "Space" : event.key;
+    code.textContent = event.code
+    keyCode.textContent = event.keyCode
+    
+});
